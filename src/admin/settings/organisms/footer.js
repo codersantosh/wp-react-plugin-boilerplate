5be5346c89fcfaf32c74c598fa921db9c97246bf
codersantosh <codersantosh@gmail.com>
2022-07-24 23:02:50 +0545

    Added: Atom save-btn

--- src/admin/settings/organisms/footer.js
+++ src/admin/settings/organisms/footer.js
@@ -1,29 +1,15 @@
 /*WordPress*/
-import {useContext} from "@wordpress/element";
-import {__} from "@wordpress/i18n";
 import {
-    CardFooter,
-    Button
+    CardFooter
 } from "@wordpress/components";
 
-/*Inbuilt Context*/
-import { SettingsContext } from '../../../context/SettingsContext';
+/*Inbuilt Components*/
+import SaveBtn from "../atoms/save-btn";
 
 const SettingsFooter = () => {
-    const { useUpdateSettings, isPending, useCanSave } = useContext(SettingsContext);
     return (
         <CardFooter>
-            <Button
-                className="button"
-                onClick={() =>
-                    useUpdateSettings()
-                }
-                isPrimary
-                disabled={isPending || !useCanSave}
-            >
-                {useCanSave?__( 'Save Settings','wp-react-plugin-boilerplate' ):__( 'Saved','wp-react-plugin-boilerplate' )}
-                {isPending?<Spinner />:''}
-            </Button>
+            <SaveBtn />
         </CardFooter>
     );
 };
